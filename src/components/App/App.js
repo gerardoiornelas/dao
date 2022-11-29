4eefa741be0ffdc081d568f995f2552150c67bbf
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2022-11-29 17:43:02 -0600

    add rowcol component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Typography, Container } from "@mui/material";
+import { Grid, Box, Typography, Container } from "@mui/material";
 import { ethers } from "ethers";
 
 // Components
@@ -7,14 +7,15 @@ import { Navigation } from "../Navigation";
 import { Loading } from "../Loading";
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import DAO_ABI from "../../abis/DAO.json";
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from "../../config.json";
 
 function App() {
   const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState(0);
+  const [dao, setDao] = useState(0);
+  const [treasuryBalance, setTreasuryBalance] = useState(0);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -22,17 +23,24 @@ function App() {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
+    // Initiate Contract
+    const dao = new ethers.Contract(
+      config[31337].dao.address,
+      DAO_ABI,
+      provider
+    );
+    setDao(dao);
+
+    // fetch treasurey balance
+    let treasuryBalance = await provider.getBalance(dao.address);
+    treasuryBalance = ethers.utils.formatUnits(treasuryBalance, 18);
+    setTreasuryBalance(treasuryBalance);
     // Fetch accounts
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
     const account = ethers.utils.getAddress(accounts[0]);
     setAccount(account);
-
-    // Fetch account balance
-    let balance = await provider.getBalance(account);
-    balance = ethers.utils.formatUnits(balance, 18);
-    setBalance(balance);
 
     setIsLoading(false);
   };
@@ -47,21 +55,12 @@ function App() {
     <Box>
       <Navigation account={account} />
 
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Container>
-          <Typography variant="h3" align="center">
-            React Hardhat Template
-          </Typography>
-          <Typography align="center">
-            <strong>Your ETH Balance:</strong> {balance} ETH
-          </Typography>
-          <Typography align="center">
-            Edit App.js to add your code here.
-          </Typography>
-        </Container>
-      )}
+      <Container>
+        <Typography variant="h3" align="center">
+          Welcome to our DAO
+        </Typography>
+        {isLoading ? <Loading /> : <></>}
+      </Container>
     </Box>
   );
 }
