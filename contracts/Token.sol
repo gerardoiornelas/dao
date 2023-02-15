145cdba60c749fe94973109f23a69dd2b3c9a246
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2023-02-15 15:26:28 -0600

    add dao contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,11 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract Token {
-    string public name = "Gerardo I. Ornelas Token";
-    string public symbol = "GIO";
+    string public name;
+    string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
@@ -34,14 +36,18 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
+
         _transfer(msg.sender, _to, _value);
+
         return true;
     }
 
     function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
+
         balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
+
         emit Transfer(_from, _to, _value);
     }
 
@@ -50,7 +56,9 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         require(_spender != address(0));
+
         allowance[msg.sender][_spender] = _value;
+
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -60,13 +68,13 @@ contract Token {
         address _to,
         uint256 _value
     ) public returns (bool success) {
-        // Check approval
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
 
         allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
         _transfer(_from, _to, _value);
+
         return true;
     }
 }
