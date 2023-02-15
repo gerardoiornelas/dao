a70ef06bb9a9439271c7dc1a6cb4918d196754b8
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2023-02-15 15:42:36 -0600

    add create proposal ts

---
+++ src/components/CreateProposal/CreateProposal.test.ts
@@ -0,0 +1,14 @@
+
+  import React from 'react';
+  import { render } from '@testing-library/react';
+  import CreateProposal from './CreateProposal';
+
+  describe('<CreateProposal/> spec', () => {
+   
+    test('should render <CreateProposal/>', () => {
+      render(<CreateProposal/>);
+    });
+
+  });
+
+  
